2f83b8208ebe912d074c10a5d6a32afeb0ad2d96
Hasan Khan <hassankhankabacha@gmail.com>
2019-10-11 21:25:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,9 +4,4 @@ def square_array(array)
   return new_array
 end
 
-arr = [1, 2, 3, 4, 5]
-square_array(arr)
-# //arr.each { |a| print a -= 10, " " }
-# # prints: -9 -8 -7 -6 -5
-#=> [1, 2, 3, 4, 5]
 
